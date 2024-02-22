28b992915193a9e1631300cb10b5a329616a7d20
Joseph Nagy <NAGYJE@ETSU.EDU>
2024-02-22 13:44:00 -0500

    .

--- BucStop/Controllers/AccountController.cs
+++ BucStop/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
 using System.Security.Claims;
 using System.Text.RegularExpressions;
 
@@ -22,10 +24,10 @@ namespace BucStop.Controllers
         {
             if (Regex.IsMatch(email, @"\b[A-Za-z0-9._%+-]+@etsu\.edu\b"))
             {
-                // Joe Nagy - code generation goes here
+                // Send email
+                string code = "abcdef"; // REMOVE THIS
+                SendEmail(email, code);
 
-                //
-                
                 // If authentication is successful, create a ClaimsPrincipal and sign in the user
                 // ClaimsPrincipal is used to create a cookie to store the user's log in information
                 var claims = new[]
@@ -56,6 +58,37 @@ namespace BucStop.Controllers
             return RedirectToAction("Login");
         }
 
+        public void SendEmail(string email, string code)
+        {
+            // Gmail credentials (replace with your Gmail address and password)
+            string senderEmail = "bucstop24@gmail.com";
+            string senderPassword = "akbr aulz okmf lack";
 
+            // Recipient's email address
+            string recipientEmail = email;
+
+            // Create and configure the SMTP client
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+            // Create the email message
+            MailMessage mail = new MailMessage(senderEmail, recipientEmail);
+            mail.Subject = "Login to BucStop";
+            mail.Body = "Welcome to BucStop!\n\nHere is your login code: " + code;
+
+            try
+            {
+                // Send the email
+                smtpClient.Send(mail);
+                Console.WriteLine("Test email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to send test email. Error: " + ex.Message);
+            }
+
+        }
     }
 }
