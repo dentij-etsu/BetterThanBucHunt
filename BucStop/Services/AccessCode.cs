19648802ee5e34d0663664cc6d2e47fb2c44bf41
dentij-etsu <111798237+dentij-etsu@users.noreply.github.com>
2024-02-26 19:25:06 -0500

    commented code

--- BucStop/Services/AccessCode.cs
+++ BucStop/Services/AccessCode.cs
@@ -9,7 +9,9 @@
         public string code;
         public DateTime time;
 
-
+        /// <summary>
+        /// Non-parameterized constructor. This is initilized with an undefined email if used.
+        /// </summary>
         public AccessCode()
         {
             time = DateTime.Now;
@@ -20,18 +22,29 @@
 
             email = "NO EMAIL DEFINED";
         }
-
+        /// <summary>
+        /// parameterized contructor
+        /// </summary>
+        /// <param name="email"> The user's email that will be attached to the access code </param>
         public AccessCode(string email)
         {
             time = DateTime.Now;
-            time = time.AddMinutes(expirationTime);
+            time = time.AddMinutes(0.5);
 
             Random generator = new Random();
             code = generator.Next(0, 1000000).ToString("D6");
 
             this.email = email;
         }
-
+        /// <summary>
+        /// This method compares the expiration time of the current access code object against the current system time,
+        /// to see if the access code is expired.
+        /// </summary>
+        /// <returns> true if the access code is expired </returns>
+        public bool isExpired ()
+        {
+            return time <= DateTime.Now;
+        }
 
     }
 }
